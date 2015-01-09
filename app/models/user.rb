68737067d888c3dbf75bfade88cf48dfa1886874
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-01-09 23:15:03 +0100

    Validation for username field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  USERNAME_REGEX = /\A\w{3,}\z/
+
+  validates :username, presence: true,
+                    format: { with: USERNAME_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
