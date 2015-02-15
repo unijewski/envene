4893dd46d795f45d94691ca4b7d15a7c9e168f6e
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-02-16 00:22:49 +0100

    Improved username regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  USERNAME_REGEX = /\A\w{3,}\z/
+  USERNAME_REGEX = /\A[A-Za-z0-9][[_|\.]?[A-Za-z0-9]+]{2,19}\z/
 
   validates :username, presence: true,
                        format: { with: USERNAME_REGEX },
