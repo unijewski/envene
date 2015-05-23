179f951075d868e336c1da159e5b6208071aabe1
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-24 01:15:30 +0200

    Add task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ActiveRecord::Base
   belongs_to :status, class_name: 'TaskStatus'
   belongs_to :priority, class_name: 'TaskPriorityType'
   belongs_to :author, class_name: 'User'
+
+  validates :name, :description, presence: :true
+  validates :progress, numericality: {
+                         greater_than_or_equal_to:0,
+                         less_than_or_equal_to: 100
+                       }
 end
