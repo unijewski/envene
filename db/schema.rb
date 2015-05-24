a77ae70e35175d2c5fb3245416f4285113c40087
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-24 18:05:10 +0200

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524141822) do
+ActiveRecord::Schema.define(version: 20150524160427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "post_categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.boolean  "published"
+    t.integer  "author_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
