03c2553cb843228c85541aa92b42f47b9aa83824
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-30 19:57:40 +0200

    Fix task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,10 @@ class Task < ActiveRecord::Base
   belongs_to :priority, class_name: 'TaskPriorityType'
   belongs_to :author, class_name: 'User'
 
-  validates :name, :description, presence: :true
-  validates :progress, numericality: {
-                         greater_than_or_equal_to:0,
+  validates :name, :description, presence: true
+  validates :progress, numericality:
+                       {
+                         greater_than_or_equal_to: 0,
                          less_than_or_equal_to: 100
                        }
 end
