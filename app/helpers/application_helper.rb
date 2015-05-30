49f22c7ea4b12de6932dd54e3513dc789a7819d4
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-30 23:37:50 +0200

    Update current_path in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def current_path(path)
     current_route = Rails.application.routes.recognize_path(path)
-    'active-menu' if current_page?(path) || params[:controller] == current_route[:controller]
+    destination = params[:controller].split('/').last.split('_').first.pluralize
+    if current_page?(path) ||
+       params[:controller] == current_route[:controller] ||
+       current_route[:controller].include?(destination)
+      'active-menu'
+    end
   end
 end
