0f9fb9523867358942660e83a20d7997c51d7608
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-31 00:12:16 +0200

    Change datatype for body in posts

---
+++ db/migrate/20150530220919_change_data_type_for_body_in_posts.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeForBodyInPosts < ActiveRecord::Migration
+  def change
+    change_column :posts, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524222251) do
+ActiveRecord::Schema.define(version: 20150530220919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150524222251) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.boolean  "published",   default: false
     t.integer  "author_id"
     t.integer  "category_id"
