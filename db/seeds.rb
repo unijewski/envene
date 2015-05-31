7fd8f43c2f9a9cb2ba0b22795ea6490d60fcd75d
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-31 02:27:12 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,18 @@ UserGroup.create(
   [{ name: 'User' }, { name: 'Administrator' }, { name: 'Moderator' }, { name: 'Bloger' }]
 )
 
+TaskStatus.create(
+  [{ name: 'New' }, { name: 'In progress' }, { name: 'Done' }, { name: 'Closed' }, { name: 'Rejected' }]
+)
+
+TaskPriorityType.create(
+  [{ name: 'Low' }, { name: 'Normal' }, { name: 'High' }, { name: 'Urgent' }, { name: 'Immediate' }]
+)
+
+PostCategory.create(
+  [{ name: 'Movies' }, { name: 'Music' }, { name: 'Travels' }, { name: 'Food' }, { name: 'Clothes' }, { name: 'Books' }]
+)
+
 User.create(
   username: 'Admin',
   email: 'admin@admin.com',
@@ -23,10 +35,21 @@ User.create(
   User.create(username: username, email: email, password: password)
 end
 
-TaskStatus.create(
-  [{ name: 'New' }, { name: 'In progress' }, { name: 'Done' }, { name: 'Closed' }, { name: 'Rejected' }]
-)
+80.times do
+  name = Faker::Lorem.sentence
+  description = Faker::Lorem.paragraph(4)
+  Task.create(
+    name: name,
+    description: description,
+    progress: 0.step(100, 10).to_a.sample,
+    status_id: rand(1..5),
+    priority_id: rand(1..5),
+    author_id: rand(1..8)
+  )
+end
 
-TaskPriorityType.create(
-  [{ name: 'Low' }, { name: 'Normal' }, { name: 'High' }, { name: 'Urgent' }, { name: 'Immediate' }]
-)
+60.times do
+  title = Faker::Lorem.sentence
+  body = Faker::Lorem.paragraph(7)
+  Post.create(title: title, body: body, author_id: rand(1..8), category_id: rand(1..5))
+end
