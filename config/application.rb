b7a0df7dc888d8302b164c4af2c46ab5575f9dfd
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-06-07 00:46:41 +0200

    Set default language as English

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Envene
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :pl
+    config.i18n.default_locale = :en
     config.i18n.available_locales = [:pl, :en]
 
     # Fonts
