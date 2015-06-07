3653e41e8fa180bc13f18a2a7362dffbd3b96222
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-06-07 21:18:00 +0200

    Fix regex of name fields

--- app/models/task_priority_type.rb
+++ app/models/task_priority_type.rb
@@ -1,7 +1,7 @@
 class TaskPriorityType < ActiveRecord::Base
   has_many :tasks, foreign_key: 'priority_id', dependent: :destroy
 
-  NAME_REGEX = /\A[A-Z][\w]+(\s[\w]+)*\z/
+  NAME_REGEX = /\A[\w]+([\s]?[\w]+)*\z/
 
   validates :name, presence: true,
                    format: { with: NAME_REGEX }
